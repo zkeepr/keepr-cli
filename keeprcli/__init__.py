abaaddb522865ff4c26a2767db84731bb8f7cbe4
KDUser <128939316+KDUser12@users.noreply.github.com>
2025-11-23 19:09:24 +0100

    Initial project

---
+++ keeprcli/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" Keepr-CLI
+
+Keeper makes it easy to manage all your development projects, in a simple and
+secure way. The project is developed primarily in Python.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This version of Keepr is command line only.
+"""
+
+__version__ = "1.0"
