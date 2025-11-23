af79c7a7e740af25be115b6c0c68dd8781f27209
KDUser <128939316+KDUser12@users.noreply.github.com>
2025-11-23 19:53:53 +0100

    Add operating system compatibility check

--- keeprcli/__main__.py
+++ keeprcli/__main__.py
@@ -20,6 +20,7 @@ logger = logging.getLogger('main')
 
 from __init__ import __version__
 from utils.pypixz_pro import install_modules, install_requirements
+from utils._os import os_compatibility
 
 try:
     import yaml
@@ -47,6 +48,7 @@ def argparse_setup() -> argparse.Namespace:
     
     parser = argparse.ArgumentParser(description=f"Keepr-CLI - v{__version__}")
     parser.add_argument('-v', '--version', action='version', version=f"Keepr-CLI - v{__version__}")
+    parser.add_argument('--skip-check', nargs='+', choices=['os', 'env', 'pip', 'all'], help="pass the program launch checks")
     parser.add_argument('--debug', action='store_true', help="enable debug output")
     args = parser.parse_args()
     
@@ -75,6 +77,29 @@ def logging_setup(args: argparse.Namespace) -> logging.Logger:
     return logger
 
 
+def technology_verification(args: argparse.Namespace, parser: argparse.Namespace) -> bool:
+    technology = {
+        'os': {
+            'name': 'operating system',
+            'function': lambda: os_compatibility()
+        }
+    }
+    
+    skipped_checks = args.skip_check or []
+    if 'all' in skipped_checks:
+        if skipped_checks and (len(skipped_checks) > 1 or skipped_checks[0] != 'all'):
+            parser.error("The 'all' argument must be the first and only choice after '--skip-check'.")
+        return True
+    
+    for object, data in technology.items():
+        if object not in skipped_checks:
+            logger.debug(f"Running {data['name']} compatibility check...")
+            data['function']()
+        else:
+            logger.warning(f"Skipping {data['name']} compatibility check.")
+    return True
+
+
 def initialize() -> None:
     keepr_path_default = get_keepr_path()
     parser, args = argparse_setup()
@@ -84,7 +109,8 @@ def initialize() -> None:
         configuration_file = yaml.safe_load(file)
         dictConfig(configuration_file)
     
-    logger = logging_setup(args=args)
+    logger = logging_setup(args)
+    technology_verification(args, parser)
     
     return None
     
---
+++ keeprcli/utils/_os.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import platform
+import re
+
+import logging
+logger = logging.getLogger("main")
+
+
+def parse_version(version: str) -> tuple:
+    return tuple(map(int, re.findall(r"\d+", version)))  # '10.0.19044' -> (10, 0, 19044)
+
+
+def os_compatibility() -> None:
+    """os_compatibility Check OS compatibility.
+
+    This feature automatically detects the operating system (Windows, macOS/Darwin,
+    or Linux), analyzes its version, and then compares it with the minimum 
+    recommended version. If the operating system or its version does not meet the 
+    minimum criteria, the program is terminated with a critical error message via 
+    the logger.
+
+    Raises:
+        Exception: If the Darwin version is lower than the minimum required version.
+        Exception: If the OS version is lower than the minimum required version.
+    """
+    
+    os_name = platform.system()
+    os_release = platform.release()
+    os_version = extract_kernel_version(os_release) if os_name == "Linux" else platform.version()
+
+    logger.debug(f"Detected operating system: {os_name}")
+    logger.debug(f"Detected release: {os_release}")
+    logger.debug(f"Detected version: {os_version}")
+    
+    # Checking supported operating systems
+    # Windows   : 10 21H2               : 10.0.19044
+    # Darwin    : macOS 12 (Monterey)   : (12)
+    # Linux     : 5.15.0-60-generic     : (5,15,0)
+    supported_os = {"Windows": {"min_version": (10, 0, 19044), "patterns": os_version},
+                    "Darwin": {"min_version": 21, "patterns": os_release},
+                    "Linux": {"min_version": (5, 10), "patterns": os_release}}
+    
+    if os_name not in supported_os:
+        exit(logger.critical(f"Unsupported operating system: {os_name}"))
+        
+    # Checking supported the OS version
+    os_info = supported_os[os_name]
+    os_version_tuple = parse_version(os_info["patterns"])
+    
+    logger.debug(f"Minimum required version for {os_name}: {os_info["min_version"]}")
+    
+    try:
+        if os_name == "Darwin":
+            if os_version_tuple[0] < os_info["min_version"]:
+                raise Exception()
+        elif os_version_tuple < os_info["min_version"]:
+            raise Exception()
+    except:
+        exit(logger.critical(f"Minimum required version for {os_name} is {os_info["min_version"]}, but you have {os_version_tuple}")) 
+        
+    logger.info(f"Operating system: {os_name} {os_version} ({os_release})")
+    
+    return None
+
+
+def extract_kernel_version(os_release: str) -> str:
+    """Extracts the kernel version from the os_release string.
+
+    Arguments:
+        os_release {str} -- The original OS release string.
+
+    Returns:
+        str -- The extracted version in the format X.Y.Z or X.Y.
+    """
+
+    # Find the first instance of the version in X.Y.Z or X.Y format.
+    match = re.search(r'\d+\.\d+(\.\d+)?', os_release)
+    if match:
+        kernel_version = match.group(0)
+        logger.debug(f"Extracted kernel version: {kernel_version}")
+        return kernel_version
+    else:
+        logger.error(f"Could not extract kernel version from: {os_release}")
+        return os_release  # Return the original version if no correspondence is found
+    
