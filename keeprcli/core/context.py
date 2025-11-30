a2ff2d6605254a56fa6d7443d1853b3e334cbd2a
KDUser <128939316+KDUser12@users.noreply.github.com>
2025-11-30 12:44:07 +0100

    Add logger

--- keeprcli/core/context.py
+++ keeprcli/core/context.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 from argparse import ArgumentParser, Namespace
 
+from .logger import logger
 
 class Context:
     def __init__(self, version: str, os: str, parameters: list[Path | ArgumentParser | Namespace]) -> None:
@@ -11,3 +12,6 @@ class Context:
         self.keepr_path_default = parameters[0]
         self.parser = parameters[1]
         self.args = parameters[2]
+
+        logger.debug(f"Context initialized (version={self.version}, os={self.os},"
+                    f"keepr_path_default={self.keepr_path_default}, parser={self.parser}, args={self.args})")
